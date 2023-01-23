de537e1181345ba411f7846a6f70f84cfaa8f2a2
Irinaa89 <108801921+Irinaa89@users.noreply.github.com>
2023-01-23 20:06:14 +0300

    Update dbconnect.php

--- php/dbconnect.php
+++ php/dbconnect.php
@@ -1,10 +1,11 @@
 <?php
 
-$db_host = 'localhost';
-$db_username = 'root';
-$db_name = 'traffic_assistant';
+$db_host = 'std-mysql.ist.mospolytech.ru';
+$db_username = 'std_2012_traffic_assistant';
+$db_password = '12345678';
+$db_name = 'std_2012_traffic_assistant';
 
-$connect = mysqli_connect($db_host, $db_username, "", $db_name);
+$connect = mysqli_connect($db_host, $db_username, $db_password, $db_name);
 
 if (!$connect) {
     die(mysqli_connect_error());
